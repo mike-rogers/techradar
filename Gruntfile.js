98a8ee5b98a2db240de26c754a036c97f4ec46f5
R. Michael Rogers <r.m.rogers@gmail.com>
2015-02-14 14:58:51 -0500

    updating to use npm/grunt, reorganized code, updated tests to account for -0 != 0 in new jasmine library

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+	jasmine: {
+	    pivotal: {
+		src: 'src/**/*.js',
+		options: {
+		    specs: 'spec/*-spec.js',
+		    helpers: 'spec/*-helper.js'
+		}
+	    }
+	},
+	lodash: {
+	    build: {
+		dest: 'build/lodash.build.js',
+		options: {
+		    modifier: 'modern'
+		}
+	    }
+	}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-lodash');
+    grunt.loadNpmTasks('grunt-if-missing');
+
+    grunt.registerTask('build-lodash', ['if-missing:lodash:build:dest']);    
+    grunt.registerTask('default', ['build-lodash', 'jasmine']);
+};
